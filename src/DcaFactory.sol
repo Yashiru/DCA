293780befcb784d7e84c5e69bd0af6ab6f20ffa2
Leo <leo.fasano@nested.finance>
2022-06-28 23:48:35 +0200

    added cron string support

--- src/DcaFactory.sol
+++ src/DcaFactory.sol
@@ -1,28 +1,27 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.6;
 
+import "@chainlink/contracts/src/v0.8/libraries/internal/Cron.sol";
 import "@chainlink/contracts/src/v0.8/factories/CronUpkeepFactory.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
-import "@chainlink/contracts/src/v0.8/libraries/internal/Cron.sol";
 
 contract DcaFactory {
     CronUpkeepFactory public cronUpKeepFactory;
     LinkTokenInterface public linkToken;
-    uint256 counter;
+    uint256 public counter;
 
     constructor(address _cronUpKeepFactory, address _linkToken) {
         cronUpKeepFactory = CronUpkeepFactory(_cronUpKeepFactory);
         linkToken = LinkTokenInterface(_linkToken);
     }
 
-    function createDcaPosition(Spec calldata spec) public {
-        cronUpKeepFactory.newCronUpkeepWithJob(
-            abi.encode(
-                address(this),
-                bytes4(keccak256(bytes("performDca()"))),
-                spec
-            )
-        );
+    function createDcaPosition(string memory cronString) public {
+        Spec memory spec = Cron.toSpec(cronString);
+        cronUpKeepFactory.newCronUpkeepWithJob(abi.encode(
+            address(this), 
+            bytes4(keccak256("performDca()")), 
+            spec
+        ));
     }
 
     function performDca() public {
