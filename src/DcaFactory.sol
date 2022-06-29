86c51dd057c1aa5124a54ea8c163697cd1136ded
Leo <leo.fasano@nested.finance>
2022-06-29 16:07:06 +0200

    feat: added automatic upkeep registration

--- src/DcaFactory.sol
+++ src/DcaFactory.sol
@@ -4,24 +4,53 @@ pragma solidity 0.8.6;
 import "@chainlink/contracts/src/v0.8/libraries/internal/Cron.sol";
 import "@chainlink/contracts/src/v0.8/factories/CronUpkeepFactory.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
+import "@chainlink/contracts/src/v0.8/KeeperRegistrar.sol";
 
 contract DcaFactory {
     CronUpkeepFactory public cronUpKeepFactory;
+    KeeperRegistrar public keeperRegistrar;
     LinkTokenInterface public linkToken;
     uint256 public counter;
 
-    constructor(address _cronUpKeepFactory, address _linkToken) {
+    constructor(
+        address _cronUpKeepFactory,
+        address _keeperRegistrar,
+        address _linkToken
+    ) {
         cronUpKeepFactory = CronUpkeepFactory(_cronUpKeepFactory);
+        keeperRegistrar = KeeperRegistrar(_keeperRegistrar);
         linkToken = LinkTokenInterface(_linkToken);
     }
 
     function createDcaPosition(string memory cronString) public {
-        Spec memory spec = Cron.toSpec(cronString);
-        cronUpKeepFactory.newCronUpkeepWithJob(abi.encode(
-            address(this), 
-            bytes4(keccak256("performDca()")), 
-            spec
-        ));
+        bytes memory encodedJob = cronUpKeepFactory.encodeCronJob(
+            address(this),
+            abi.encodePacked(bytes4(keccak256("performDca()"))),
+            cronString
+        );
+        cronUpKeepFactory.newCronUpkeepWithJob(encodedJob);
+        linkToken.transferFrom(msg.sender, address(this), 5 ether);
+
+        bytes memory data = abi.encodeWithSignature(
+            "register(string,bytes,address,uint32,address,bytes,uint96,uint8)",
+            "DCA",
+            abi.encode("email"),
+            address(this),
+            5000000,
+            msg.sender,
+            "",
+            5 ether,
+            0
+        );
+
+        linkToken.transferAndCall(address(keeperRegistrar), 5 ether, data);
+    }
+
+    function withdrawLink() public {
+        linkToken.transfer(
+            0xdB75A7BE5BD568dA00ad2A949Eb9cF966759FA41,
+            linkToken.balanceOf(address(this))
+        );
     }
 
     function performDca() public {
--- test/DcaFactory.sol
+++ test/DcaFactory.sol
@@ -5,17 +5,30 @@ import "forge-std/Test.sol";
 import "../src/DcaFactory.sol";
 import "@chainlink/contracts/src/v0.8/libraries/internal/Cron.sol";
 import "@chainlink/contracts/src/v0.8/factories/CronUpkeepFactory.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/LinkTokenInterface.sol";
 
 contract ContractTest is Test {
     DcaFactory factory;
+    LinkTokenInterface link;
+    uint256 UINT256_MAX = 115792089237316195423570985008687907853269984665640564039457584007913129639935;
+
+
     function setUp() public {
+        link = LinkTokenInterface(0x84b9B910527Ad5C03A9Ca831909E21e236EA7b06);
+        
+        vm.prank(0xE4dDb4233513498b5aa79B98bEA473b01b101a67);  
         factory = new DcaFactory(
-            0x7E206B547953BcE9a94B80c28F3f8a869e2D82f9,
-            0x326C977E6efc84E512bB9C30f76E30c160eD06FB
+            0xe5aC9480a27C01A8851cD041A206047A4C01723A,
+            0x6179B349067af80D0c171f43E6d767E4A00775Cd,
+            address(link)
         );
+        
+        vm.prank(0xE4dDb4233513498b5aa79B98bEA473b01b101a67);    
+        link.approve(address(factory), UINT256_MAX);
     }
 
     function testCreateDcaPosition() public {
+        vm.prank(0xE4dDb4233513498b5aa79B98bEA473b01b101a67);  
         factory.createDcaPosition("*/5 * * * *");
     }
 }
